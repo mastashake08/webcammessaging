d7b68ec2236865bda713e8b252c6b5d5bacf441c
Jyrone Parker <mastashake08@users.noreply.github.com>
2016-05-11 13:59:24 -0700

    Update VideoMessageController.php

--- app/Http/Controllers/VideoMessageController.php
+++ app/Http/Controllers/VideoMessageController.php
@@ -64,7 +64,7 @@ class VideoMessageController extends Controller
         //
         //$stream = new \App\VideoStream(storage_path('app/').VideoMessage::find($id)->location);
         //$stream->start(); 
-        return Storage::get(VideoMessage::find($id)->location)
+        return Storage::get(VideoMessage::find($id)->location);
     }
 
     /**
